318bce12d227a1bdcd7ae49ed3a5c84f12c0a013
Marcus Vinnicus <88518763+omarcus212@users.noreply.github.com>
2022-09-13 16:55:00 -0300

    chore : add SpaceBackground

---
+++ web/src/components/SpaceBackground/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../../styles/components/SpacebackgroundLogin/index.scss";
+
+
+
+const SpaceBackground = () =>{
+
+    return(
+
+  <>
+  <div className="backSpace">
+        <span>
+         
+        </span>
+  </div>
+
+  </>
+
+    );
+}
+
+export default SpaceBackground;
---
+++ web/src/pages/Login/Login.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import SpaceBackground from "../../components/SpaceBackground";
+
+const PageLogin = () =>{
+
+    return(
+   
+        <>
+        <main id="LoginPage">
+
+        <SpaceBackground/>   
+
+        </main>
+         
+        </>
+  
+    );
+}
+
+export default PageLogin;
