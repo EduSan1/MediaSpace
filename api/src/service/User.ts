c148745c4599519b128332baad3b608d96fabb28
EDUARDO SANTOS NASCIMENTO <21276323@dsn127m1249386.cfp127aluno.local>
2022-09-14 16:57:41 -0300

    chore : add mail, cpf and nickname verification

--- api/src/service/User.ts
+++ api/src/service/User.ts
@@ -5,51 +5,59 @@ import { UserORM } from "../entity/User"
 import { PhoneRepository } from "../repository/Phone"
 
 export class UserService {
-    private _ : any
-    private phoneRepo : any
+    private _: any
+    private phoneRepo: any
 
-    constructor(repo : any) {
+    constructor(repo: any) {
         this._ = repo,
-        this.phoneRepo = new PhoneRepository()
+            this.phoneRepo = new PhoneRepository()
     }
 
-    create  = async (entity: UserORM) => {
+    create = async (entity: UserORM) => {
         try {
-            const cpf =  await this._.listByVar( "cpf" , entity.cpf)
-            const nickname = await await this._.listByVar( "nickname" , entity.nickname)
-            const mail = await await this._.listByVar( "mail" , entity.mail)
+            // console.log(parseInt(entity.cpf))
 
-            // if (mail !== null) 
+            // if (parseInt(entity.cpf) !== 11 || parseInt(entity.cpf) === NaN) {
             //     return {
-            //         message: "email já cadastrado",
-            //         statusCode : 200
+            //         message: "insira um cpf válido!",
+            //         statusCode: 400
             //     }
-            
-            // if (nickname !== null) 
-            //     return {
-            //         message: "nickname já cadastrado",
-            //         statusCode : 200
-            //     }
+            // }
+            const cpf = await this._.listByVar("cpf", entity.cpf)
+            const nickname = await await this._.listByVar("nickname", entity.nickname)
+            const mail = await await this._.listByVar("mail", entity.mail)
 
-        //     const hashPassword = await bcrypt.hash(entity.password , 10)
-        //     entity.password = hashPassword
-        //     const user : UserORM =  await this._.create(entity)
+            if (mail !== null || nickname !== null || cpf !== null)
+                return {
+                    message: "Não foi possivel cadastrar o usuário pois alguns dados ja estão cadastrados",
+                    mail: mail !== null ? true : false,
+                    cpf: cpf !== null ? true : false,
+                    nickname: nickname !== null ? true : false,
+                    statusCode: 400
+            }
+     
 
-        //     let phone : PhoneORM = entity.phone
-        //     phone = {...phone, user : user}
-        //     await this.phoneRepo.create(phone)
-            
+            const hashPassword = await bcrypt.hash(entity.password, 10)
+            entity.password = hashPassword
+            const user: UserORM = await this._.create(entity)
 
-        //     return {
-        //         message: "Usuário cadastrado com sucesso!",
-        //         statusCode : 201
-        //     }
+            if (entity.phone) {
+                let phone: PhoneORM = entity.phone
+                phone = { ...phone, user: user }
+                await this.phoneRepo.create(phone)
+            }
+
+
+            return {
+                message: "Usuário cadastrado com sucesso!",
+                statusCode: 201
+            }
 
         } catch (error) {
             return {
                 message: error.message,
                 error: error.code,
-                statusCode : 400
+                statusCode: 400
             }
         }
     }
@@ -61,7 +69,7 @@ export class UserService {
             return {
                 message: error.message,
                 error: error.code,
-                statusCode : 400
+                statusCode: 400
             }
         }
     }
@@ -69,17 +77,17 @@ export class UserService {
     list = async () => {
         try {
             return await this._.list()
-          
+
         } catch (error) {
             return {
                 message: error.message,
                 error: error.code,
-                statusCode : 400
+                statusCode: 400
             }
         }
     }
 
-    update = async (id : string , entity : UserORM) => {
+    update = async (id: string, entity: UserORM) => {
         try {
             const entityExists = await this._.listById(id)
 
@@ -101,7 +109,7 @@ export class UserService {
             return {
                 message: error.message,
                 error: error.code,
-                statusCode : 400
+                statusCode: 400
             }
         }
     }
@@ -113,13 +121,13 @@ export class UserService {
 
             return {
                 message: "Dados removidos com sucesso",
-                statusCode : 200
+                statusCode: 200
             }
         } catch (error) {
             return {
                 message: error.message,
                 error: error.code,
-                statusCode : 400
+                statusCode: 400
             }
         }
 
