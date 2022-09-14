72e000d2a6716371e1ab6bc392d76b260dd2bc3a
Marcus Vinnicus <88518763+omarcus212@users.noreply.github.com>
2022-09-14 16:56:25 -0300

    chore : add SpaceLogin

---
+++ web/src/components/SpaceBackground/Login/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+const LoginSpace = () =>{
+  
+    return(
+
+     <>
+       
+       <main className="SpaceLogin">
+
+            <div>
+                 <img src="../assets/img/rocketart.png" alt="" />
+            </div>    
+            
+            <div>
+                <p>oii</p>
+            </div>        
+        
+
+  
+       </main>
+     
+     </>
+
+    );
+
+}
+
+export default LoginSpace;
--- web/src/components/SpaceBackground/index.tsx
+++ web/src/components/SpaceBackground/index.tsx
@@ -1,22 +1,29 @@
 import React from "react";
 import "../../styles/components/SpacebackgroundLogin/index.scss";
+import "../../styles/components/SpacebackgroundLogin/indexLogin.scss"
+import LoginSpace from "./Login";
 
 
+const SpaceBackground = () => {
 
-const SpaceBackground = () =>{
+  return (
 
-    return(
-
-  <>
-  <div className="backSpace">
-        <span>
-         
+    <>
+      <div className="backSpace">
+        <span className="MediaSpaceLogo">
+          <img src="../assets/img/MediaSpaceLogo.png" />
         </span>
-  </div>
+        
+        <LoginSpace/>
+      
+        <span className="Space">
+          <img src="../assets/img/backSpace.png" />
+        </span>
+      </div>
 
-  </>
+    </>
 
-    );
+  );
 }
 
 export default SpaceBackground;
