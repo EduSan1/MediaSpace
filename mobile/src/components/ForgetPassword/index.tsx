8013576f0b1243c3558e8ba1ca252e71a62b774f
MARIA FERNANDA BENTO DA SILVA <21276378@dsn127m1249389.cfp127aluno.local>
2022-09-21 14:08:17 -0300

    chore : base login component

---
+++ mobile/src/components/ForgetPassword/index.tsx
@@ -0,0 +1,84 @@
+import React, {useState} from "react"
+import { Text, View, StyleSheet, TextInput, Button, TouchableOpacity, ScrollView } from "react-native";
+
+export const ForgetPassword = () => {
+
+
+    return (
+        <>
+        <View style={style.container}>
+        <Text style={style.title}>Esqueceu a senha?</Text>
+        <Text style={style.text}>Enviaremos um e-mail para a autenticação e recuperação de senha</Text>
+        <TextInput style={style.input}/>
+        <TouchableOpacity 
+        style={style.button}
+        activeOpacity={0.8}>
+            <Text style={style.buttonText}>Enviar</Text>
+        </TouchableOpacity>
+        </View>
+        </>
+    )
+}
+
+const style = StyleSheet.create({
+    section: {
+        flex: 1,
+        height: "75%",
+        width: "100%",
+        backgroundColor: '#1A2345',
+        justifyContent: "flex-end",
+        alignItems: "center"
+    },
+    header : {
+        width: "100%",
+        height: "40%",
+        
+    },
+    icon: {
+        width: 140,
+        height: 140,
+
+    },
+    container: {
+        width: "100%",
+        height: "80%",
+        display: "flex",
+        alignContent: "flex-start",
+        justifyContent: "flex-start",
+        alignItems:"center"
+    },
+    title:{
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 10,
+        color: "#B275FF"
+    },
+    text:{
+        fontSize: 12,
+        width: "55%",
+        color:'#46307B',
+        marginBottom:12,
+        display: 'flex',
+    }, 
+    input:{},
+    button:{
+        width: 250,
+        height: 50,
+        paddingVertical: 8,
+        borderRadius: 10,
+        backgroundColor: "#B275FF",
+        justifyContent:'center',
+        alignItems:'center',
+        alignSelf:'center',
+        marginTop: 50,
+        
+        
+    },
+    buttonText:{
+        fontSize: 20,
+        fontWeight:"bold",
+        color: "#fff",
+
+    }
+
+})
--- mobile/src/layers/login.tsx
+++ mobile/src/layers/login.tsx
@@ -1,22 +1,27 @@
 import React from "react";
 import { Text, SafeAreaView, View, StyleSheet, Image } from "react-native";
 import WavyBackground from "react-native-wavy-background";
+import { ForgetPassword } from "../components/ForgetPassword";
 export default function Login() {
     return (
         <SafeAreaView>
-            <View>
+            <View style={style.header}>
                 <View style={style.section}>
-                <Image style={style.icon} source={require('../../assets/icons/MediaSpaceLogoWhite.png')} />
+                    <Image style={style.icon} source={require('../../assets/icons/MediaSpaceLogoWhite.png')} />
                 </View>
-            <WavyBackground
-                        bottom={false}
-                        height={100}
-                        width={100}
-                        amplitude={13}
-                        frequency={10}
-                        offset={20}
-                        color="#1A2368"
-                        />
+                <WavyBackground
+                    bottom={false}
+                    height={100}
+                    width={100}
+                    amplitude={13}
+                    frequency={10}
+                    offset={20}
+                    color="#1A2368"
+                />
+            </View>
+
+            <View style={style.container}>
+                <ForgetPassword/>
             </View>
 
         </SafeAreaView>
@@ -24,15 +29,30 @@ export default function Login() {
 }
 const style = StyleSheet.create({
     section: {
-        height: "50%",
+        height: "75%",
         width: "100%",
         backgroundColor: '#1A2368',
-        justifyContent:"flex-end",
-        alignItems:"center"
+        justifyContent: "flex-end",
+        alignItems: "center"
     },
     icon: {
-        width:140,
-        height:140,
+        width: 140,
+        height: 140,
 
     },
+    header : {
+        width: "100%",
+        height: "40%",
+        
+    },
+    container: {
+        width: "100%",
+        height: "60%",
+        display: "flex",
+        position:"relative",
+        alignContent: "center",
+        justifyContent: "center",
+        alignItems:"center"
+        
+    }
 })
