c45703f6f43204b5ef532764e049e44c14ce6f2c
EDUARDO SANTOS NASCIMENTO <21276323@dsn127m1249386.cfp127aluno.local>
2022-09-26 16:47:26 -0300

    fix : fix error name

--- mobile/src/components/utils/LoginInput/index.tsx
+++ mobile/src/components/utils/LoginInput/index.tsx
@@ -8,24 +8,24 @@ interface ILoginInput {
     name: string
     iconName: string
     isPassword?: boolean
-    error: boolean
+    hasError: boolean
     handleChange: (text: string, name: string) => void
     onClickIcon?: () => void
 }
 
-export const LoginInput = ({ title, value, name, iconName, handleChange, isPassword, onClickIcon, error}: ILoginInput) => {
+export const LoginInput = ({ title, value, name, iconName, handleChange, isPassword, onClickIcon, hasError}: ILoginInput) => {
 
     const [inputTitle, serInputTitle] = useState(false)
 
     return (
 
-        <View style={[styles.container , error ? { borderColor: "#FF6666" } : { borderColor: "#D3C5F8" }]}>
+        <View style={[styles.container , hasError ? { borderColor: "#FF6666" } : { borderColor: "#D3C5F8" }]}>
             {inputTitle ? <Text style={styles.inputTitle} >{title}</Text> : null}
             <View style={styles.inputContainer}>
                 <TextInput secureTextEntry={isPassword} value={value} onChangeText={(text) => handleChange(text, name)} onFocus={() => serInputTitle(true)} placeholder={inputTitle ? "" : title} style={styles.input}></TextInput>
 
                 <Pressable style={styles.inputIcon} onPress={() => onClickIcon ? onClickIcon() : null}>
-                    <Icon size={Dimensions.get('window').height * 0.025} name={iconName} style={[styles.inputIcon , error ? { color: "#FF6666" } : { color: "#46307B" } ]} />
+                    <Icon size={Dimensions.get('window').height * 0.025} name={iconName} style={[styles.inputIcon , hasError ? { color: "#FF6666" } : { color: "#46307B" } ]} />
                 </Pressable>
 
             </View>
