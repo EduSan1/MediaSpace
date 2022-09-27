2f9095bf50faec0c6808499218aba560de75d0e3
LAISE OLIVEIRA SILVA <21276363@dsn127m1247444.cfp127aluno.local>
2022-09-27 16:35:55 -0300

    chore: get getting data from input

--- web/src/components/Register/index.tsx
+++ web/src/components/Register/index.tsx
@@ -1,39 +1,44 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import InputLogin from "../utils/Input/LoginInput";
 import { FaUserAlt } from "react-icons/fa";
 import { MdPhone } from "react-icons/md";
 import { HiIdentification } from "react-icons/hi";
 
-
-// 
-
-// const [errors, setErrors] = React.useState({});
-
-// const handlerErrors = (errorMenssage: string, input: string) => {
-//     setErrors((prevState) => ({ ...prevState, [input]: errorMenssage }));
-// }
-
-// const validate = () => {
-//     let validate = true
-
-//     if (!inputs.first_name || !inputs.last_Name || !inputs.nickname || !inputs.cpf || !inputs.mail || !inputs.password) {
-//         validate = false
-//         handlerErrors("Preencha os campos obrigatótrios", "first_name" )
-//     }
-// }
-
 const RegisterSpace = () => {
+
+    const [inputs, setInputs] = React.useState({
+        first_name: '',
+        last_Name: '',
+        nickname: '',
+        birth_date: '',
+        cpf: '',
+        mail: '',
+        password: '',
+        biography: '',
+    })
+    
+    const handleChange = (event : React.ChangeEvent<HTMLInputElement>) =>{
+        setInputs({
+            ...inputs, [event.target.name] : event.target.value
+        })
+    }
+    
+     //useEffect(() => {
+      //  console.log(inputs)
+    //}, [inputs])
+
+
     return (
         <>
-            <div className="pageRegister">
-                <div className="titlePage" >
+            <div className="page_register">
+                <div className="title_page" >
                     <h1>Faça seu Cadastro</h1>
                 </div>
-                <span className="containerInputs">
-                    <span className="teste">
-                        <InputLogin valueLogin="teste" hasError={false} handleChange={() => {}} typeInput={"text"} placeholder={"Nome"} icon={<FaUserAlt className="IconLogin" />} name={"first_name"} label={"Nome"} className={"inputRegister"} />
-                    </span>
-                </span>
+                <div className="container_inputs">
+                    <div className="teste">
+                        <InputLogin valueLogin={inputs.first_name} hasError={false} handleChange={(event: React.ChangeEvent<HTMLInputElement>) => {handleChange(event)}} typeInput={"text"} placeholder={"Nome"} icon={<FaUserAlt className="IconLogin" />} name={"first_name"} label={"Nome"} className={"inputRegister"} />
+                    </div>
+                </div>
             </div>
 
 
--- web/src/components/SpaceBackground/index.tsx
+++ web/src/components/SpaceBackground/index.tsx
@@ -15,8 +15,8 @@ const SpaceBackground = () => {
         </span>
 
         <main className="SpaceLogin">
-         {/* <RegisterSpace />*/}
-           <LoginSpace /> 
+         <RegisterSpace />
+          {/* <LoginSpace />*/} 
         </main>
 
 
