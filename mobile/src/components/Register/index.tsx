7066bd87a121bea52a069a893896d3034e61e655
MARIA FERNANDA BENTO DA SILVA <21276378@dsn127m1249389.cfp127aluno.local>
2022-09-28 16:51:43 -0300

    fix : merge

--- mobile/src/components/Register/index.tsx
+++ mobile/src/components/Register/index.tsx
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
-import { StyleSheet,View, ScrollView,Text,Dimensions, Image } from "react-native";
+import { StyleSheet, View, ScrollView, Text, Dimensions,Image } from "react-native";
+import { CheckBoxComponent} from "@react-native-community/checkbox";
 import { LoginButton } from "../utils/LoginButton";
 import { LoginInput } from "../utils/LoginInput";
 
 export const Register = () => {
 
     const [userRegister, setRegister] = useState({
-        name:"",
-        surname:"",
-        mail:"",
-        password:"",
-        nickname:"",
-        cpf:"",
-        birth:"",
-        cell:"",
-        genre:"",
-        biography:"",
+        name: "",
+        surname: "",
+        mail: "",
+        password: "",
+        nickname: "",
+        cpf: "",
+        birth: "",
+        cell: "",
+        genre: "",
+        biography: "",
+        image:"",
     })
 
     const [hasError, setHasError] = useState(false)
@@ -25,7 +27,7 @@ export const Register = () => {
     const handleChange = (text: string, name: string) => {
         setRegister(
             {
-                ... userRegister,
+                ...userRegister,
                 [name]: text
             }
         )
@@ -33,113 +35,144 @@ export const Register = () => {
     const changeVisibilityPassword = () => {
         setVisibilityPassword(!visibilityPassword)
     }
-    
-    const confirm = async () =>{
+
+    const confirm = async () => {
         console.log("confirmado")
     }
 
 
-    return(
-    <>
-        <ScrollView 
-            style={styles.container}
-            horizontal={true} 
-            pagingEnabled={true} 
-            showsHorizontalScrollIndicator={true}>
+    return (
+        <>
+            <ScrollView
+                style={styles.container}
+                horizontal={true}
+                pagingEnabled={true}
+                showsHorizontalScrollIndicator={true}>
+
+                <View style={styles.View}>
+                    <View style={styles.View2}>
+                        <LoginInput name="name" iconName="person-outline" value={userRegister.name} handleChange={handleChange} error={hasError} title="Nome" />
+                        <LoginInput name="surname" iconName="person-outline" value={userRegister.surname} handleChange={handleChange} error={hasError} title="Sobrenome" />
+                        <LoginInput name="mail" iconName="mail-outline" value={userRegister.mail} handleChange={handleChange} error={hasError} title="E-mail" />
+                        <LoginInput onClickIcon={changeVisibilityPassword} isPassword={visibilityPassword} name="password" error={hasError} iconName={visibilityPassword ? "lock-outline" : "lock-open"} value={userRegister.password} handleChange={handleChange} title="Senha" />
+                    </View>
+
+                    <View style={styles.scrollView}>
+                        <Image style={styles.scrollIcon} source={require("../../../assets/img/scrollhint.png")} />
+                        <Image style={styles.scrollIcon} source={require("../../../assets/img/scrollhint.png")} />
+                    </View>
+                </View>
+
 
             <View style={styles.View}>
-                <LoginInput name="name" iconName="person-outline" value={userRegister.name} handleChange={handleChange} error={hasError} title="Nome"/>
-                <LoginInput name="surname" iconName="person-outline" value={userRegister.surname} handleChange={handleChange} error={hasError} title="Sobrenome"/>
-                <LoginInput name="mail" iconName="mail-outline" value={userRegister.mail} handleChange={handleChange} error={hasError} title="E-mail"/>
-                <LoginInput onClickIcon={changeVisibilityPassword} isPassword={visibilityPassword} name="password" error={hasError} iconName={visibilityPassword ? "lock-outline" : "lock-open"} value={userRegister.password} handleChange={handleChange} title="Senha" />                    
+                <View style={styles.View2}>
+                    <LoginInput name="nickname" iconName="person-outline" value={userRegister.nickname} handleChange={handleChange} error={hasError} title="Nickname" />
+                    <LoginInput name="cpf" iconName="person-outline" value={userRegister.cpf} handleChange={handleChange} error={hasError} title="CPF" />
+                    <LoginInput name="birth" iconName="today" value={userRegister.birth} handleChange={handleChange} error={hasError} title="Data de nascmento" />
+                    <LoginInput name="cell" iconName="phone" value={userRegister.cell} handleChange={handleChange} error={hasError} title="Celular" />
+
+                    <View style={styles.scrollView}>
+                        <Image style={styles.scrollIcon} source={require("../../../assets/img/scrollhint.png")} />
+                        <Image style={styles.scrollIcon} source={require("../../../assets/img/scrollhint.png")} />
+                    </View>
+                </View>
             </View>
 
-            <View style={styles.scrollView}>
-                    <Image style={styles.scroll} source={require("../../../assets/img/scrollhint.png")}/>
-                    <Image style={styles.scroll} source={require("../../../assets/img/scrollhint.png")}/>
-            </View>
-           
 
             <View style={styles.View}>
-                <LoginInput name="nickname" iconName="person-outline" value={userRegister.nickname} handleChange={handleChange} error={hasError} title="Nickname"/>
-                <LoginInput name="cpf" iconName="person-outline" value={userRegister.cpf} handleChange={handleChange} error={hasError} title="CPF"/>
-                <LoginInput name="birth" iconName="today" value={userRegister.birth} handleChange={handleChange} error={hasError} title="Data de nascmento"/>
-                <LoginInput name="cell" iconName="phone" value={userRegister.cell} handleChange={handleChange} error={hasError} title="Celular"/>
-            
-            </View>
-
-            <View style={styles.scrollView}>
-                    <Image style={styles.scroll} source={require("../../../assets/img/scrollhint.png")}/>
-                    <Image style={styles.scroll} source={require("../../../assets/img/scrollhint.png")}/>
+                <View style={styles.View2}>
+                    
+                    <LoginInput name="genre" iconName="person-outline" value={userRegister.genre} handleChange={handleChange} error={hasError} title="Genero" />
+                    <LoginInput name="biography" iconName="person-outline" value={userRegister.biography} handleChange={handleChange} error={hasError} title="Biografia" />
+                    
+                    {/* <View style={styles.scrollView}>
+                        <Image style={styles.scrollIcon} source={require("../../../assets/img/scrollhint.png")} />
+                        <Image style={styles.scrollIcon} source={require("../../../assets/img/scrollhint.png")} />
+                    </View> */}
+                </View>
             </View>
 
             <View style={styles.View}>
-                <LoginInput name="genre" iconName="person-outline" value={userRegister.genre} handleChange={handleChange} error={hasError} title="Genero"/>
-                <LoginInput name="biography" iconName="person-outline" value={userRegister.biography} handleChange={handleChange} error={hasError} title="Biografia"/> 
+                <View style={styles.View2}>
+                    <LoginInput name="image" iconName="person-outline" value={userRegister.image} handleChange={handleChange} error={hasError} title="imagem" />
+                </View>
             </View>
 
+            </ScrollView>
 
-        </ScrollView>
-
-        <View style={styles.containerTextButton}>
+            <View style={styles.containerTextButton}>
                 <Text style={styles.text}>Arraste para o lado para preencher todos os campos </Text>
                 <LoginButton type="dark" action={confirm} title="Continuar" />
                 <Text style={styles.text2}>Já possui uma conta? Entre</Text>
-        </View>
-    </>
+            </View>
+        </>
     )
-    
+
 }
 
 const styles = StyleSheet.create({
-
-    View:{
-        width: Dimensions.get('window').width ,
-        minHeight: Dimensions.get('window').height * 0.100,
+    container: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height * 0.1,
+        flex: 1
+    },
+    View: {
+        width: Dimensions.get('window').width,
+        minHeight: Dimensions.get('window').height * 0.1,
         display: "flex",
-        alignItems:'center',
-        justifyContent:'space-evenly'
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        flexDirection: "row",
     },
-    text:{
+    View2: {
+        width: Dimensions.get('window').width,
+        minHeight: Dimensions.get('window').height * 0.20,
+        display: "flex",
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        
+    },
+    text: {
         fontSize: 10,
-        width: "55%",
-        color:'#46307B',
+        width: Dimensions.get('window').width * 0.5,
+        color: '#46307B',
         display: 'flex',
-        marginBottom:10,
+        marginBottom: 10,
     },
-    containerTextButton:{
-        width:"100%",
-        height: 10,
-        justifyContent:"flex-start",
-        alignItems:"center",
+    containerTextButton: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height ,
+        justifyContent: "flex-start",
+        alignItems: "center",
         display: 'flex',
         flex: 2,
-        backgroundColor: "#234312"
     },
-    container:{
-        width: Dimensions.get('window').width ,
-        height: Dimensions.get('window').height * 0.1,
-        backgroundColor:"#34f344"
-    },
-    text2:{
-        width: Dimensions.get('window').width ,
+
+    text2: {
+        width: Dimensions.get('window').width * 0.5,
         display: "flex",
-        fontSize: 16,
-        fontWeight:"bold",     
-        color:'#B275FF',
+        fontSize: 12,
+        fontWeight: "bold",
+        color: '#B275FF',
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 5
+    },
+    scrollIcon: {
+        width: Dimensions.get('window').width * 0.085 ,
+        height: Dimensions.get('window').height * 0.06,
+        justifyContent:"space-between",
         alignItems:"center",
-        justifyContent:"center",
-        padding:5
+        
+
     },
-    scroll:{
-        width: Dimensions.get('window').width * 0.1 ,
+    scrollView: {
+        width: Dimensions.get('window').width * 1.9,
         height: Dimensions.get('window').height * 0.1,
-    },
-    scrollView:{
-        minHeight: Dimensions.get('window').height * 0.100,
         display: "flex",
         alignItems: "center",
         justifyContent: "space-between",
-        flexDirection: "column"
+        flexDirection: "column",
+        position: "absolute"
     }
 })
