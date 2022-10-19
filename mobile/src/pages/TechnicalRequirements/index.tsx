8d4ae8c428d6480e0c1e0633b3c61557cb6bc678
Geeansouza <souzaz_eg@outlook.com>
2022-10-19 16:49:10 -0300

    chore:component Technical

--- mobile/src/pages/TechnicalRequirements/index.tsx
+++ mobile/src/pages/TechnicalRequirements/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Text, SafeAreaView, View, StyleSheet, Image, ScrollView, Dimensions } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { BtnBackPage } from "../../components/utils/BtnBackPage";
-
+import { CardRequirements } from "../../components/utils/CardRequirements";
 export default function TechnicalRequirements() {
     return (
         <>
@@ -12,25 +12,19 @@ export default function TechnicalRequirements() {
                     <View style={style.titleSection}>
                         <Text style={style.text}>Requisitos técnicos - Nome do projeto</Text>
                     </View>
-                    {/* Requisitos */}
-                    <View style={style.sectionProject}>
-                        <View style={style.textIcon}>
-                            <Image style={style.icon} source={require('../../../assets/icons/checkedImage.png')}/>
-                            <Text style={style.textStatusRequeriments}>01 - Entregue</Text>
-                        </View>
-                        <Text style={style.titleRequeriments}>Layout</Text>
-                        <Text style={style.TextRequirements}>O layout define como o app inteiro será representado bla bla bla </Text>
-                        <View style={style.cardRequirements}>
-                            <Text style={style.TextCardRequirements}>Porcentagem do valor: 25%</Text>
-                            <Text style={style.TextCardRequirements}> Valor = R$ 500,00</Text>
-                        </View>
-                    </View>
-
-
-                    <View style={style.sectionProject}></View>
-                    <View style={style.sectionProject}></View>
-                    <View style={style.sectionProject}></View>
-                    <View style={style.sectionProject}></View>
+                    <CardRequirements/>
+                    <CardRequirements/>
+                    <CardRequirements/>
+                    <CardRequirements/>
+                    <CardRequirements/>
+                    <CardRequirements/>
+                    <CardRequirements/>
+                   <Text style={style.textValue}>Valor total do projeto: R$ 2.000,00</Text>
+                    <View style={style.footerProject}>
+                        <Text>requisitos</Text>
+                        <Text>Caso esteja interessado em fazer mudanças ou adaptações nos requisitos, solicite uma revisão, só é permitido a edição assim que o cliente e o(s) prestador(es) aceitarem a solicitação.</Text>
+                    </View>                    
+ 
                 </SafeAreaView>
             </ScrollView>
         </>
@@ -48,57 +42,30 @@ const style = StyleSheet.create({
     text: {
         color:"#75A5FF",
         marginStart: 20,
-        fontSize: 24,
+        fontSize: Dimensions.get('window').height * 0.03,
         fontWeight: "900",
         marginTop: Dimensions.get('window').height * 0.12,
+    },
+    textValue: {
+        color:"#75A5FF",
+        marginStart: 20,
+        fontSize: Dimensions.get('window').height * 0.027,
+        fontWeight: "900",
+        marginTop: Dimensions.get('window').height * 0.02,
     },
     titleSection: {
         width: Dimensions.get('window').width,
         height: Dimensions.get('window').height * 0.20,
         marginBottom:Dimensions.get('window').height * 0.03,
-        // backgroundColor:"blue",
+
     },
-    sectionProject: {
+    footerProject: {
         width: Dimensions.get('window').width * 0.90,
         height: Dimensions.get('window').height * 0.22,
         alignSelf:"center",
         marginVertical: 20,
         // backgroundColor: "blue",
     },
-    textStatusRequeriments:{
-        fontSize:18,
-        color:"#75A5FF",
-        fontWeight:"300",
-        paddingLeft:10,
-    },
-    textIcon:{
-        flexDirection:"row"
-    },
-    icon:{
-        width: Dimensions.get('window').width * 0.06,
-        height: Dimensions.get('window').width * 0.06,
-    },
-    titleRequeriments:{
-        marginLeft:Dimensions.get('window').width * 0.08,
-        fontSize:18,
-    },
-    TextRequirements:{
-        marginLeft:Dimensions.get('window').width * 0.08,
-        fontSize:15,
-        color:"#808080",
-    },
-    cardRequirements:{
-        marginLeft:Dimensions.get('window').width * 0.08,
-        width: Dimensions.get('window').width * 0.70,
-        height: Dimensions.get('window').height * 0.10,
-        backgroundColor:"#CECCFD",
-        borderRadius:10,
-    },
-    TextCardRequirements:{
-        marginTop:10,
-        marginLeft:Dimensions.get('window').width * 0.02,
-        fontSize:17,
-        color:"#756DE6",
-    }
+
 
 })
