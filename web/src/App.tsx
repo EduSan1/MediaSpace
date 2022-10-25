11a4c587d91732aac764c92a71463844df2ac577
Marcus Vinnicus <88518763+omarcus212@users.noreply.github.com>
2022-10-25 13:56:13 -0300

    fix:merge

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -18,7 +18,8 @@ import Projects from './pages/Projects';
 import Eventes from './pages/Eventes';
 import Menssagens from './pages/Menssage';
 import Perfil from './pages/Perfil';
-import ProjectsrequirementsFreelancer from './pages/ProjectRequirementsPage'
+import ProjectsrequirementsFreelancer from './pages/ProjectRequirementsPage';
+import AllFreelancerInteration from './pages/AllProjectInterasion';
 
 import Teste from './pages/teste';
 //import SpaceBackground from './components/SpaceBackground';
@@ -48,7 +49,8 @@ function App() {
         <Route path='/Eventes' element={<Eventes />} />
         <Route path='/Menssagens' element={<Menssagens />} />
         <Route path='/Perfil' element={<Perfil />} />
-        <Route path='/projects/testeView' element={<ProjectsrequirementsFreelancer />} />
+        <Route path='/projects/requirementsFreelancer' element={<ProjectsrequirementsFreelancer />} />
+        <Route path='/project/all-Interation'  element={<AllFreelancerInteration/>}/>
         
 
 
