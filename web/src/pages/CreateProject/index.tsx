5bd5d437f01d99720d4782537baae9a83b52cf68
laise <132laise@gmail.com>
2022-10-25 22:52:15 -0300

    chore: ajustment details of project page

--- web/src/pages/CreateProject/index.tsx
+++ web/src/pages/CreateProject/index.tsx
@@ -276,141 +276,123 @@ const CreateProject = () => {
             <NavegationBar />
             <div className="Container">
                <SearchBar />
-               <section className="section_main">
-                  <div className="container_page_create_project">
-                     <div></div>
-                     <div className="page_create_project">
-                        <div className="container_informations">
-                           <div className="title_page_project" >
-                              <h1>Criação de projeto</h1>
-                           </div>
-                           <InputProject label={"Nome do projeto"} maxLenght={100} name={"name"} handleChange={(event: React.ChangeEvent<HTMLInputElement>) => { handleChange(event) }} />
+               <section className="container_page_create_project">
+                  <div className="page_create_project">
+                     <div className="container_informations">
+                        <div className="title_page_project" >
+                           <h1>Criação de projeto</h1>
+                        </div>
+                        <InputProject label={"Nome do projeto"} maxLenght={100} name={"name"} handleChange={(event: React.ChangeEvent<HTMLInputElement>) => { handleChange(event) }} />
 
-                           <div className="container_description_project">
-                              <label className="subtitulo_projects">Descrição <span> * </span></label>
-                              <div>
-                                 <textarea onChangeCapture={(event: React.ChangeEvent<HTMLTextAreaElement>) => { numberCaracteres(event) }} maxLength={800} name={"description"} onChange={({ target }) => { project.description = target.value }} />
-                                 <span>{caracteres.caracteres}/800</span>
-                              </div>
-
+                        <div className="container_description_project">
+                           <label className="subtitulo_projects">Descrição <span> * </span></label>
+                           <div>
+                              <textarea onChangeCapture={(event: React.ChangeEvent<HTMLTextAreaElement>) => { numberCaracteres(event) }} maxLength={800} name={"description"} onChange={({ target }) => { project.description = target.value }} />
+                              <span>{caracteres.caracteres}/800</span>
                            </div>
 
-                           <div className="container_categories_projects">
-                              <div className="subtitulo_projects">
-                                 <label> Categorias </label>
-                                 <p className="paragraph_projects">Selecione a categoria do projeto</p>
-                              </div>
-                              <div className="categories projects">
-                                 {
-                                    categories.map((category: any) => {
-                                       return <ButtonCategories category={category.name} name={category} icon="" id={category.id} key={category.id} action={() => console.log("")} setSubCategories={findSubCategories} />
-                                    })
-                                 }
+                        </div>
 
-
-                              </div>
+                        <div className="container_categories_projects">
+                           <div className="subtitulo_projects">
+                              <label> Categorias </label>
+                              <p className="paragraph_projects">Selecione a categoria do projeto</p>
                            </div>
+                           <div className="categories projects">
+                              {
+                                 categories.map((category: any) => {
+                                    return <ButtonCategories category={category.name} name={category} icon="" id={category.id} key={category.id} action={() => console.log("")} setSubCategories={findSubCategories} />
+                                 })
+                              }
 
-                           <div className="container_subacategories_projects">
-                              <div className="subtitulo_projects">
-                                 <label> Sub-categorias </label>
-                                 <p className="paragraph_projects">Selecione a sub-categoria do projeto</p>
-                              </div>
-                              <div className="sub_categories">
-                                 {
-                                    subcategoriesToRender.map((category: any) => {
-                                       return category.sub_categories.map((subCategory: any) => {
-                                          return <Checkbox nameOption={subCategory.name} onClickFunction={(check) => check ? removeSubcategory(subCategory.id) : addToProject(subCategory.id, "sub_categories")} />
-                                       })
-                                    })
-                                 }
-                              </div>
+
                            </div>
                         </div>
 
-                        <div className="container_informations">
-                           <div className="container_input_project">
-                              <label className="subtitulo_projects">  Prazo estimado de entrega <span> * </span></label>
-                              <div>
-                                 <input type="date" maxLength={100} name="estimated_deadline" onChange={(event: React.ChangeEvent<HTMLInputElement>) => { handleChange(event) }} />
-                              </div>
-                              <div>
-                                 <label className="paragraph_projects">Obs: Sugerimos que essa data seja uma estimativa crível de acordo com seu projeto,
-                                    você pode negociá-la com um prestador depois.</label>
-                              </div>
+                        <div className="container_subacategories_projects">
+                           <div className="subtitulo_projects">
+                              <label> Sub-categorias </label>
+                              <p className="paragraph_projects">Selecione a sub-categoria do projeto</p>
                            </div>
-
-                           <div className="container_input_project">
-                              <label className="subtitulo_projects">Valor estimado (BRL)<span> * </span></label>
-                              <div>
-                                 <input className="input_value_project" type="number" min={0} name="estimated_value" onChange={(event: React.ChangeEvent<HTMLInputElement>) => { handleChange(event) }} />
-                              </div>
-                              <span>
-                                 <label className="paragraph_projects">Obs: Sugerimos um valor mínimo de R$15,00. Você pode negociá-lo com um prestador depois.</label>
-                              </span>
+                           <div className="sub_categories">
+                              {
+                                 subcategoriesToRender.map((category: any) => {
+                                    return category.sub_categories.map((subCategory: any) => {
+                                       return <Checkbox nameOption={subCategory.name} onClickFunction={(check) => check ? removeSubcategory(subCategory.id) : addToProject(subCategory.id, "sub_categories")} />
+                                    })
+                                 })
+                              }
                            </div>
+                        </div>
 
-                           <div className="container_files">
-                              <div className="container_text">
-                                 <label className="subtitulo_projects">Imagens</label>
-                                 <div>
-                                    <p className="paragraph_projects">Imagens de referências ao projeto</p>
-                                    <span className="paragraph_projects">0/4</span>
-                                 </div>
-
-                              </div>
-
-                              <div className="container_images">
-                                 <div className="aligment_images">
-                                    <div className="images">
-                                       <img src="" />
-                                    </div>
-                                    <div className="images">
-                                       <img src="" />
-                                    </div>
-                                 </div>
-
-                                 <div className="aligment_images" >
-                                    <div className="images">
-                                       <img src="" />
-                                    </div>
-                                    <div className="images">
-                                       <img src="" />
-                                    </div>
-                                 </div>
-                              </div>
+                        <div className="container_input_project">
+                           <label className="subtitulo_projects">  Prazo estimado de entrega <span> * </span></label>
+                           <div>
+                              <input type="date" maxLength={100} name="estimated_deadline" onChange={(event: React.ChangeEvent<HTMLInputElement>) => { handleChange(event) }} />
                            </div>
+                           <div>
+                              <label className="paragraph_projects">Obs: Sugerimos que essa data seja uma estimativa crível de acordo com seu projeto,
+                                 você pode negociá-la com um prestador depois.</label>
+                           </div>
+                        </div>
 
-
-                           <div className="container_boost">
-                              <label className="subtitulo_projects"> Impulsionamento <span> (recurso pago) </span></label>
-                              <p className="paragraph_projects">Tenha um alcance maior com sua publicação</p>
-                              <div className="teste">
-                                 <img src="./boosted.png" />
-                                 <div className="container_boost_buttons">
-                                    <BoostButton type={"Padrão"} valueBoost={"Gratuito"} imageBoost={"../../../public/assets/img/project/free.png"} label={""} />
-                                    <BoostButton type={"Impulsionado"} valueBoost={"R$ 50,00"} imageBoost={"../../../public/assets/img/project/boosted.png"} label={""} />
-                                 </div>
-                                 <div className="container_texts_boost">
-                                    <p>Com a opção "impulsionado" você tem a sua publicação divulgada com um maior alcance, sendo anunciada nos primeiros resultados de exibição na plataforma. </p>
-                                    <p>Incluídos no Impulsionamento
-                                       <ul>
-                                          <li>Pagamento único (uma vez para cada publicação)</li>
-                                          <li>Maior visibilidade</li>
-                                          <li>Destaque na exibição</li>
-                                       </ul>
-                                    </p>
-                                    <span></span>
-                                    <p>Recomendado para grandes projetos.</p>
-                                 </div>
-
-                              </div></div>
+                        <div className="container_input_project">
+                           <label className="subtitulo_projects">Valor estimado (BRL)<span> * </span></label>
+                           <div>
+                              <input className="input_value_project" type="number" min={0} name="estimated_value" onChange={(event: React.ChangeEvent<HTMLInputElement>) => { handleChange(event) }} />
+                           </div>
+                           <span>
+                              <label className="paragraph_projects">Obs: Sugerimos um valor mínimo de R$15,00. Você pode negociá-lo com um prestador depois.</label>
+                           </span>
                         </div>
                      </div>
-                     <div className="aligment_button">
-                        <InputBtn typeInput={'submit'} name={'btnCadastrar'} className={'input_btn_publicar_project'} valueBtn={'Publicar'} onClick={() => { }} />
+
+                     <div className="container_informations">
+
+
+                        <div className="container_files">
+                           <div className="container_text">
+                              <label className="subtitulo_projects">Imagens</label>
+                              <div>
+                                 <p className="paragraph_projects">Imagens de referências ao projeto</p>
+                                 <span className="paragraph_projects">0/4</span>
+                              </div>
+
+                           </div>
+
+                           <div className="container_images">
+                              <div className="aligment_images">
+                                 <div className="images">
+                                    <img src="" />
+                                 </div>
+                                 <div className="images">
+                                    <img src="" />
+                                 </div>
+                              </div>
+
+                              <div className="aligment_images" >
+                                 <div className="images">
+                                    <img src="" />
+                                 </div>
+                                 <div className="images">
+                                    <img src="" />
+                                 </div>
+                              </div>
+                           </div>
+
+                           <div className="aligment_button">
+                              <InputBtn typeInput={'submit'} name={'btnCadastrar'} className={'input_btn_select_image'} valueBtn={'Selecionar imagem'} onClick={() => { }} />
+                           </div>
+                        </div>
+
+
+
                      </div>
                   </div>
+                  <div className="aligment_button">
+                     <InputBtn typeInput={'submit'} name={'btnCadastrar'} className={'input_btn_publicar_project'} valueBtn={'Publicar'} onClick={() => { }} />
+                  </div>
+
                </section>
             </div>
          </main>
