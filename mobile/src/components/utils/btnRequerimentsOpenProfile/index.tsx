401bde4c6b0cc1966f11c3089b7a7871c9b97707
Geeansouza <souzaz_eg@outlook.com>
2022-10-28 16:41:47 -0300

    chore:list-integration

--- mobile/src/components/utils/btnRequerimentsOpenProfile/index.tsx
+++ mobile/src/components/utils/btnRequerimentsOpenProfile/index.tsx
@@ -3,7 +3,7 @@ import { Image, Text, StyleSheet, Dimensions, Pressable, ActivityIndicator, View
 export const BtnRequirementsOpenProfile  = () => {
     return (
         <Pressable style={styles.btnStyle}>
-            <Text style={styles.text}>Abrir Perfil</Text>
+            <Text style={styles.text}>Abrir perfil</Text>
         </Pressable>
 
     )
@@ -13,6 +13,7 @@ const styles = StyleSheet.create({
 
     btnStyle:{
         borderWidth:1,
+        backgroundColor:"#FFF",
         borderColor:"#B275FF",
         width: Dimensions.get('window').width * 0.3,
         height:Dimensions.get('window').width * 0.1,
@@ -21,8 +22,9 @@ const styles = StyleSheet.create({
     },
     text: {
         textAlign:"center",
-        fontSize: Dimensions.get('window').height * 0.023,
-        fontWeight: "700",
+        fontSize: Dimensions.get('window').height * 0.020,
+        fontWeight: "600",
+        color:"#B275FF"
   
     },
 })
