e1e03791add874f54e7ee101121b681c2b595654
Gabriel Gomes <gabriielgomess@outlook.com>
2022-10-28 12:11:01 -0300

    chore: delivery routes

--- api/src/routes/Project.ts
+++ api/src/routes/Project.ts
@@ -10,10 +10,10 @@ projectRoute.get("/:projectId", projectController.getById)
 projectRoute.put("/:projectId", projectController.update)
 projectRoute.post("/disable/:projectId", projectController.disable)
 projectRoute.post("/registerInterest/:projectId", projectController.registerInterest)
-projectRoute.post("/freelancersIntersted/:projectId", () => "rota de listar os freelancer interessados  no projeto")
+projectRoute.post("/freelancersInterested/:projectId", () => "rota de listar os freelancer interessados  no projeto")
 projectRoute.post("/selectFreelancer/:projectId", projectController.selectFreelancer)
 projectRoute.post("/acceptRequirement/:projectId", projectController.acceptRequirements)
 projectRoute.post("/denyRequirement/:projectId", projectController.denyRequirements)
-projectRoute.post("/startProject/:projectRequirementId", () => "rota de confirmação de inicio de projeto")
+projectRoute.post("/startProject/:projectRequirementId", () => "rota de confirmação de inicio de projeto") 
 
 export default projectRoute
