e346fc6fd3bd7238041da960cb8ae32ad27a8703
laise <132laise@gmail.com>
2022-10-31 09:00:57 -0300

    fix: merge

--- web/src/pages/PreviewProject/Creator/index.tsx
+++ web/src/pages/PreviewProject/Creator/index.tsx
@@ -32,49 +32,49 @@ const PreviewProject = () => {
       "create_at": "",
       "update_at": "",
       "user": {
-          "id": "4b408f96-8b6c-4126-b0a7-89aa80ac10a8",
-          "first_name": "Eduardo",
-          "last_name": "Santos",
-          "nickname": "1231312",
-          "birth_date": "2022-10-10T03:00:00.000Z",
-          "cpf": "12341231901",
-          "mail": "edusan3456@gmail.com",
-          "password": "$2b$10$CpMtEbCWE40bejsW6m3unO38s2GkBKhHj01Jd/tpyDC8n84tPh6v2",
-          "biography": "",
-          "profile_picture": "teste",
-          "is_active": true,
-          "is_authenticated": false,
-          "create_at": "2022-10-24T19:15:57.396Z",
-          "update_at": "2022-10-24T19:15:57.396Z",
-          "gender": {
-              "id": "e6217fb3-6ee2-4f1e-8987-51e6622d9445",
-              "gender": "teste",
-              "create_at": "2022-09-27T17:03:18.918Z",
-              "update_at": "2022-09-27T17:03:18.918Z"
-          },
-          "phone": {
-              "id": "84464cf1-0d47-474c-99dd-df0bffd50bf9",
-              "ddd": "11",
-              "phone": "912345678",
-              "ddi": null
-          },
-          "teams": [],
-          "project_member": []
+         "id": "4b408f96-8b6c-4126-b0a7-89aa80ac10a8",
+         "first_name": "Eduardo",
+         "last_name": "Santos",
+         "nickname": "1231312",
+         "birth_date": "2022-10-10T03:00:00.000Z",
+         "cpf": "12341231901",
+         "mail": "edusan3456@gmail.com",
+         "password": "$2b$10$CpMtEbCWE40bejsW6m3unO38s2GkBKhHj01Jd/tpyDC8n84tPh6v2",
+         "biography": "",
+         "profile_picture": "teste",
+         "is_active": true,
+         "is_authenticated": false,
+         "create_at": "2022-10-24T19:15:57.396Z",
+         "update_at": "2022-10-24T19:15:57.396Z",
+         "gender": {
+            "id": "e6217fb3-6ee2-4f1e-8987-51e6622d9445",
+            "gender": "teste",
+            "create_at": "2022-09-27T17:03:18.918Z",
+            "update_at": "2022-09-27T17:03:18.918Z"
+         },
+         "phone": {
+            "id": "84464cf1-0d47-474c-99dd-df0bffd50bf9",
+            "ddd": "11",
+            "phone": "912345678",
+            "ddi": null
+         },
+         "teams": [],
+         "project_member": []
       },
       "categories": [
-          {
-              "id": "92997090-f960-45b9-9f7a-4cb9f0e430a1",
-              "name": "Jorge",
-              "icon": "aaaaaa",
-              "is_active": true,
-              "create_at": "2022-10-24T19:16:34.763Z",
-              "update_at": "2022-10-24T19:16:34.763Z"
-          }
+         {
+            "id": "92997090-f960-45b9-9f7a-4cb9f0e430a1",
+            "name": "Jorge",
+            "icon": "aaaaaa",
+            "is_active": true,
+            "create_at": "2022-10-24T19:16:34.763Z",
+            "update_at": "2022-10-24T19:16:34.763Z"
+         }
       ],
       "images": []
-  }
-)
-   
+   }
+   )
+
 
    //console.log(project.images)
 
@@ -106,7 +106,7 @@ const PreviewProject = () => {
                      <div className='container_creator_value'>
                         <div className='container_profile'>
                            <div className='picture_profile'>
-                              <img src="" />
+                              <img src={project.user.profile_picture} />
                            </div>
                            <div>
                               <label>{project.user.first_name}</label>
