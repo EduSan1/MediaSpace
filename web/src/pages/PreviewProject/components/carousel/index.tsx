ca80546c92274e941fd6c5a5eac710bde71bcef0
laise <132laise@gmail.com>
2022-11-01 11:26:48 -0300

    chore: carousel image

--- web/src/pages/PreviewProject/components/carousel/index.tsx
+++ web/src/pages/PreviewProject/components/carousel/index.tsx
@@ -10,9 +10,9 @@ export const CarouselImages = ({ images }: ICarouselImage) => {
 
    const [slideIndex, setSlideIndex] = useState(1)
 
-   const moveDot = (index:number)=>{
+   const moveDot = (index: number) => {
       setSlideIndex(index)
-      console.log(index)   
+      console.log(index)
    }
 
    return (
@@ -24,26 +24,27 @@ export const CarouselImages = ({ images }: ICarouselImage) => {
                      return (
                         <>
                            <div className={slideIndex === index + 1 ? "slide active-anim" : "slide"}>
-                              <img src={image.url}/>
+                              <img src={image.url} />
                               <p>{index}</p>
                            </div>
                         </>)
                   })
                }
+
+               <div className='container_dots'>
+                  {
+                     images.map((item, index) => {
+                        return (
+                           <div onClick={() => { moveDot(index + 1) }} className={slideIndex === index + 1 ? "dot active" : "dot"}>
+                           </div>
+                        )
+                     })
+                  }
+               </div>
+
             </div>
 
-            <div className='container_dots'>
-               {
-                  images.map((item,index) => {
-                     return (
-                        <div onClick={()=>{moveDot(index + 1)}} className={slideIndex === index + 1? "dot active" : "dot"}>
-                          <p>{index}</p>
-                        </div>
-                        // <label htmlFor="image1"> uai</label>
-                     )
-                  })
-               }
-            </div>
+
          </div>
       </>
    )
