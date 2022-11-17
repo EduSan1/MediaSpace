8e1f0b96bbded10f12ce37cbe5c499555a7ca4cc
132anderson <132anderson@gmail.com>
2022-11-16 21:12:52 -0300

    chore: preview project web

--- web/src/pages/PreviewProject/Creator/index.tsx
+++ web/src/pages/PreviewProject/Creator/index.tsx
@@ -5,7 +5,7 @@ import { formatDate, formatMoney } from '../../../service/Regex/regex';
 import InputBtn from "../../../components/utils/Button/InputBtn";
 import { RiDeleteBinLine, RiEditBoxLine } from "react-icons/ri";
 import { FaEdit } from "react-icons/fa";
-import { useParams,Link, Route, useNavigate  } from 'react-router-dom';
+import { useParams, Link, Route, useNavigate } from 'react-router-dom';
 import api from '../../../service';
 import CategoryCard from '../../../components/utils/CategoryCard';
 import { CarouselImages } from '../components/carousel';
@@ -15,88 +15,134 @@ import FreelancerInterest from '../components/FreelancersInterest/index';
 
 const PreviewProjectCreator = () => {
 
-  const navigation = useNavigate();
+   const navigation = useNavigate();
    const { projectId } = useParams()
    const [project, setProject] = useState({
-      "id": "e7049e0e-2243-4d20-987c-382fee430e2a",
-      "name": "dasdasd",
-      "description": "asdasdasd",
-      "value": 10.5,
-      "estimated_deadline": "2022-07-10T03:00:00.000Z",
+      "name": "",
+      "description": "",
+      "value": 0,
+      "estimated_deadline": "",
       "finish_project_date": null,
       "start_project_date": null,
       "is_active": true,
       "status": "AWAITING_START",
-      "create_at": "2022-10-25T19:27:34.162Z",
-      "update_at": "2022-10-25T19:27:34.162Z",
+      "create_at": "2022-11-06T19:53:48.395Z",
+      "update_at": "2022-11-06T19:53:48.395Z",
+      "interest": [
+         {
+            "id": "",
+            "all_members_accept": true,
+            "is_selected": false,
+            "team": {
+               "id": "",
+               "name": "",
+               "nickname": "123131",
+               "description": null,
+               "profile_picture": "teste",
+               "general_evaluation": 0,
+               "status": true,
+               "is_active": true,
+               "is_freelancer": true,
+               "create_at": "2022-11-16T22:59:40.702Z",
+               "update_at": "2022-11-16T22:59:40.702Z"
+            },
+            "members": [
+               {
+                  "id": "b014933b-04fe-47a9-ac0e-086b06ff9bf5",
+                  "is_active": null,
+                  "accept": true,
+                  "is_selected": false
+               }
+            ]
+         }
+      ],
+      "sub_categories": [
+         {
+            "id": "e0b772b1-c6a5-4e8e-bd4f-7836d9271c44",
+            "name": "outrotesteobrigatorio",
+            "is_active": true,
+            "create_at": "2022-11-06T19:49:08.421Z",
+            "update_at": "2022-11-06T19:49:08.421Z"
+         }
+      ],
+      "requirements": [],
+      "management": null,
       "user": {
-          "id": "4b408f96-8b6c-4126-b0a7-89aa80ac10a8",
-          "first_name": "Eduardo",
-          "last_name": "Santos",
-          "nickname": "1231312",
-          "birth_date": "2022-10-10T03:00:00.000Z",
-          "cpf": "12341231901",
-          "mail": "edusan3456@gmail.com",
-          "password": "$2b$10$CpMtEbCWE40bejsW6m3unO38s2GkBKhHj01Jd/tpyDC8n84tPh6v2",
-          "biography": "",
-          "profile_picture": "teste",
-          "is_active": true,
-          "is_authenticated": false,
-          "create_at": "2022-10-24T19:15:57.396Z",
-          "update_at": "2022-10-24T19:15:57.396Z",
-          "gender": {
-              "id": "e6217fb3-6ee2-4f1e-8987-51e6622d9445",
-              "gender": "teste",
-              "create_at": "2022-09-27T17:03:18.918Z",
-              "update_at": "2022-09-27T17:03:18.918Z"
-          },
-          "phone": {
-              "id": "84464cf1-0d47-474c-99dd-df0bffd50bf9",
-              "ddd": "11",
-              "phone": "912345678",
-              "ddi": null
-          },
-          "teams": [],
-          "project_member": []
+         "id": "78af51cf-da66-4b4d-809a-3e9f4b024a3f",
+         "first_name": "Laise",
+         "last_name": "Silva",
+         "nickname": "1231312",
+         "birth_date": "2022-10-10T03:00:00.000Z",
+         "cpf": "11122233345",
+         "mail": "132laise@gmail.com",
+         "password": "$2b$10$MG1iPGNdickwr.2FS/n9cuyR403NRyxNiHr92QdXaOtj.zUiicV5G",
+         "biography": "",
+         "profile_picture": "teste",
+         "is_active": true,
+         "is_authenticated": true,
+         "create_at": "2022-11-06T19:50:17.804Z",
+         "update_at": "2022-11-07T00:38:05.000Z",
+         "gender": {
+            "id": "572f76e9-2940-4de1-8154-090ff7ec8ab4",
+            "gender": "Teste",
+            "create_at": "2022-11-06T19:47:46.123Z",
+            "update_at": "2022-11-06T19:47:46.123Z"
+         },
+         "phone": {
+            "id": "721a6f4f-5e15-4e97-9592-437c7a2c30d8",
+            "ddd": "11",
+            "phone": "912345678",
+            "ddi": null
+         },
+         "teams": [],
+         "project_member": []
       },
       "categories": [
-          {
-              "id": "92997090-f960-45b9-9f7a-4cb9f0e430a1",
-              "name": "Jorge",
-              "icon": "aaaaaa",
-              "is_active": true,
-              "create_at": "2022-10-24T19:16:34.763Z",
-              "update_at": "2022-10-24T19:16:34.763Z"
-          }
+         {
+            "id": "a10fd9ab-7dd7-4b24-977b-291fba62de6f",
+            "name": "obrigatóerio",
+            "icon": "aaaaaa",
+            "is_active": true,
+            "create_at": "2022-11-06T19:48:24.139Z",
+            "update_at": "2022-11-06T19:48:24.139Z"
+         }
       ],
       "images": [
-          {
-              "id": "81546406-d545-4b49-acd6-9beaf743e9cb",
-              "url": "https://cdn.dribbble.com/userupload/3827623/file/original-1688f73a4463bd9fc50754e1b4f85fce.png?compress=1&resize=752x",
-              "create_at": "2022-10-25T19:27:34.172Z",
-              "update_at": "2022-10-25T19:27:34.172Z"
-          }
+         {
+            "id": "f86c28c2-a555-4199-bf50-ab2616bf2f5a",
+            "url": "https://cdn.dribbble.com/userupload/3859445/file/original-d0324ddd20a4faca55d1cb44c66d239f.jpg?compress=1&resize=1024x768",
+            "create_at": "2022-11-06T19:53:48.408Z",
+            "update_at": "2022-11-06T19:53:48.408Z"
+         }
       ]
    }
    )
 
-   console.log(project)
+   //console.log(project)
+
+   const dateFormat = (dates: any) => {
+      dates = project.create_at.split("T")
+      return dates[0]
+   }
+
+
+
    /********slide */
    const [currentIndex, setCurrent] = useState(0)
    useEffect(() => {
       api.get(`/project/${projectId}`).then((res: any) => {
          setProject(res.data.data)
-        
+
       })
-   },[])
+   }, [])
 
    useEffect(() => {
       api.get(`/project/${projectId}`).then((res: any) => {
          setProject(res.data.data)
-        
+
       })
-   },[])
-   
+   }, [])
+
    return (
       <>
          <main>
@@ -114,16 +160,16 @@ const PreviewProjectCreator = () => {
                      </div>
 
                      <div className='container_dates'>
-                        <p><span>Criado em:   </span>{formatDate(project.estimated_deadline)}</p>
+                        <p><span>Criado em:   </span>{dateFormat(project.create_at)}</p>
                         <p>
                            <span>Prazo de término:  </span>
-                           {formatDate(project.estimated_deadline)}
+                           {dateFormat(project.estimated_deadline)}
                         </p>
                      </div>
                      <div className='container_creator_value'>
                         <div className='container_profile'>
                            <div className='picture_profile'>
-                              {/* <img src={project.user.profile_picture} /> */}
+                              <img src={project.user.profile_picture} />
                            </div>
                            <div>
                               <label>{project.user.first_name}</label>
@@ -141,10 +187,10 @@ const PreviewProjectCreator = () => {
                      </div>
 
                      <div className='container_buttons_project'>
-                        <span onClick={()=>{}} > <RiDeleteBinLine/> </span>
+                        <span onClick={() => { }} > <RiDeleteBinLine /> </span>
                         <span> <FaEdit /> </span>
 
-                        <InputBtn typeInput={'submit'} name={'btnCadastrar'} className={'input_btn_project'} valueBtn={'Executar projeto'} onClick={() => { navigation("/projects/allfreelancerview")}} />
+                        <InputBtn typeInput={'submit'} name={'btnCadastrar'} className={'input_btn_project'} valueBtn={'Executar projeto'} onClick={() => { navigation(`/projects/selectFreelancer/${projectId}`) }} />
                      </div>
 
                      <div className='container_categories_project'>
@@ -161,14 +207,16 @@ const PreviewProjectCreator = () => {
                         </h1>
                         <div className='freelancers_interested'>
                            {
-                            
+
+                              project.interest.map((interest: any) => {
+                                 console.log(interest)
+                                 return <FreelancerInterest image_profile={interest.team.picture_profile} name={interest.team.name} nickname={interest.team.nickname} />
+                              })
+
                            }
-                           <FreelancerInterest image_profile={""} name={"teste"} nickname={"testeee"} />
-
-
                         </div>
 
-                        <p>Ver todos</p>
+                        <p onClick={() => { navigation(`/projects/allfreelancerview/${projectId}`) }}>Ver todos</p>
                      </div>
                   </div>
                </section>
