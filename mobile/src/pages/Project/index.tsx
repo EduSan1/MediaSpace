c655970d7c2c16bbaa9af0b11ce763105eebf9d9
Eduardo Santos Nascimento <eduardosantosnascimento@iMac-de-Eduardo.local>
2022-11-28 17:01:12 -0300

    chore : add fluxo

--- mobile/src/pages/Project/index.tsx
+++ mobile/src/pages/Project/index.tsx
@@ -13,7 +13,7 @@ import BtnBackPage from "../../components/utils/BtnBackPage"
 
 interface IProject {
     navigation: any
-    route : any
+    route: any
 }
 
 export const Project = ({ navigation, route }: IProject) => {
@@ -21,13 +21,13 @@ export const Project = ({ navigation, route }: IProject) => {
         navigation.navigate(screen)
     }
 
-    
+
 
     const dateMask = (value: string) => {
         return value
-            .split("T")[0].replace(/(\d{4})-(\d{2})-(\d{2})/,"$1/$2/$3")
+            .split("T")[0].replace(/(\d{4})-(\d{2})-(\d{2})/, "$1/$2/$3")
     }
-    const {projectId, userId} = route.params
+    const { projectId, userId } = route.params
 
 
     const [imageIndex, setImageIndex] = useState(0)
@@ -49,7 +49,7 @@ export const Project = ({ navigation, route }: IProject) => {
         update_at: "",
         interest: [],
         sub_categories: [
-       
+
         ],
         requirements: [],
         management: "",
@@ -86,7 +86,7 @@ export const Project = ({ navigation, route }: IProject) => {
         categories: [{}],
         images: [
             {
-                url : ""
+                url: ""
             }
         ]
     })
@@ -94,14 +94,16 @@ export const Project = ({ navigation, route }: IProject) => {
     const freelancerProject = async () => {
         const projectApi = {
             ...project.user,
-            
+
         }
         console.log(projectApi)
 
+        console.log(`/project/registerInterest/${project.id}`)
+
         api.post(`/project/registerInterest/${project.id}`, {
-            frelancerId : userId
-        }).then((res:any)=>{
-            if(res.data.statusCode === 201){
+            freelancerId: userId
+        }).then((res: any) => {
+            if (res.data.statusCode === 201) {
                 navigation.navigate("WorkersAppliedPage", {
                     projectId: res.data.data.id,
                 })
@@ -112,81 +114,81 @@ export const Project = ({ navigation, route }: IProject) => {
     }
 
     useEffect(() => {
-        api.get(`/project/${projectId}`).then((res: any)=>{
+        api.get(`/project/${projectId}`).then((res: any) => {
             setProject(res.data.data)
-         })
-     }, [])
+        })
+    }, [])
 
 
     return (
         <>
             <TabBar currentScreen="Project" navigateTo={navigateTo} />
             <View style={styles.btnBack}>
-                <BtnBackPage action={()=> navigation.navigate("ListProject")}/> 
+                <BtnBackPage action={() => navigation.navigate("ListProject")} />
             </View>
-                <ScrollView style={styles.page} > 
+            <ScrollView style={styles.page} >
 
-                
-                        <ScrollImage  isActive={imageIndex == 4 ? false : true} userImage={project.images} setUserImage={(image: string)  => {} } /> 
-                
 
-                    <View style={styles.containerFilho}>
-                           <View style={styles.containerDates} >
-                            <View style={styles.conatinerDate}>
-                                <Text style={styles.titleDate}>Criado em:  </Text>
-                                <Text style={styles.date}>{dateMask(project.create_at)}</Text>
-                            </View>
+                <ScrollImage isActive={imageIndex == 4 ? false : true} userImage={project.images} setUserImage={(image: string) => { }} />
 
-                                <Text style={styles.traco}>|</Text>
 
-                            <View style={styles.conatinerDate}>
-                                <Text style={styles.titleDate}>Prazo término: </Text>
-                                <Text style={styles.date}>{dateMask(project.estimated_deadline)}</Text>
-                            </View>
+                <View style={styles.containerFilho}>
+                    <View style={styles.containerDates} >
+                        <View style={styles.conatinerDate}>
+                            <Text style={styles.titleDate}>Criado em:  </Text>
+                            <Text style={styles.date}>{dateMask(project.create_at)}</Text>
                         </View>
 
-                   <View style={styles.containerProfile}>
+                        <Text style={styles.traco}>|</Text>
+
+                        <View style={styles.conatinerDate}>
+                            <Text style={styles.titleDate}>Prazo término: </Text>
+                            <Text style={styles.date}>{dateMask(project.estimated_deadline)}</Text>
+                        </View>
+                    </View>
+
+                    <View style={styles.containerProfile}>
                         <View style={styles.containerNameImage}>
-                            <Image style={styles.image} source={{uri : project.user.profile_picture}} />
+                            <Image style={styles.image} source={{ uri: project.user.profile_picture }} />
                             <View>
-                            <Text style={styles.nameUser}>{project.user.first_name} {project.user.last_name}</Text> 
-                            <Text style={styles.nicknameUser}>@{project.user.nickname}</Text> 
+                                <Text style={styles.nameUser}>{project.user.first_name} {project.user.last_name}</Text>
+                                <Text style={styles.nicknameUser}>@{project.user.nickname}</Text>
                             </View>
                         </View >
-                            <View>
+                        <View>
                             <Text style={styles.value}>Valor estimado:</Text>
                             <Text style={styles.price}> R$ {project.value}</Text>
-                            </View>
                         </View>
+                    </View>
 
-                        <View style={styles.containerTitle}>
-                            <Text style={styles.nameProject}>{project.name}</Text>
-                            <Text style={styles.describle}>{project.description}</Text>
+                    <View style={styles.containerTitle}>
+                        <Text style={styles.nameProject}>{project.name}</Text>
+                        <Text style={styles.describle}>{project.description}</Text>
 
-                        <View style={styles.categ}>      
+                        <View style={styles.categ}>
 
-                        <Image style={styles.divisor} source={require("../../../assets/icons/divisor.png")} />              
-                                {
-                                    project.categories.map((category : any) => {
-                                        return <Text style={styles.categorySelected}>{category.name}</Text>
+                            <Image style={styles.divisor} source={require("../../../assets/icons/divisor.png")} />
+                            {
+                                project.categories.map((category: any) => {
+                                    return <Text style={styles.categorySelected}>{category.name}</Text>
 
-                                    })
-                                }
+                                })
+                            }
 
-                        <Image style={styles.divisor} source={require("../../../assets/icons/divisor.png")} />                        
-                        </View>
-
-                  
-
-                             <View style={styles.button}>
-                                <LoginButton type="dark" action={() => freelancerProject()} isLoad={projectLoad} title="Participar" />
-                            </View>
+                            <Image style={styles.divisor} source={require("../../../assets/icons/divisor.png")} />
                         </View>
 
 
 
-                    </View> 
-                </ScrollView>  
+                        <View style={styles.button}>
+                            <LoginButton type="dark" action={() => freelancerProject()} isLoad={projectLoad} title="Declarar interesse" />
+                        </View>
+                    </View>
+
+
+
+                </View>
+            </ScrollView>
 
 
         </>
@@ -221,12 +223,12 @@ const styles = StyleSheet.create({
         display: 'flex',
         padding: 20,
     },
-    containerImage:{
+    containerImage: {
         width: Dimensions.get('window').width,
         height: Dimensions.get('window').height * 0.21,
-        backgroundColor:"blue"
+        backgroundColor: "blue"
     },
-    containerNameImage:{
+    containerNameImage: {
         flexDirection: 'row',
         alignItems: 'center',
         display: 'flex',
@@ -236,59 +238,59 @@ const styles = StyleSheet.create({
         height: Dimensions.get("window").width * 0.15,
         borderRadius: 100,
     },
-    nameUser:{
+    nameUser: {
         fontSize: 14,
         fontWeight: "400",
         paddingStart: 10,
-        
+
     },
-    nicknameUser:{
+    nicknameUser: {
         fontSize: 14,
         fontWeight: "300",
         paddingStart: 10,
-        color:'#B3B3B3'
+        color: '#B3B3B3'
     },
-    value:{
+    value: {
         fontSize: 14,
         fontWeight: "400",
-        color:'#B3B3B3'
+        color: '#B3B3B3'
     },
-    price:{
+    price: {
         fontSize: 14,
         fontWeight: "500",
-        color:'#30A851'
+        color: '#30A851'
     },
-    nameProject:{
+    nameProject: {
         fontSize: 16,
         fontWeight: "bold",
     },
-    titleDate:{
+    titleDate: {
         fontSize: 14,
         fontWeight: "400",
     },
-    traco:{
+    traco: {
         fontSize: 25,
         fontWeight: "300",
-        color:'#999999',
-        
+        color: '#999999',
+
     },
-    date:{ 
+    date: {
         fontSize: 14,
         fontWeight: "400",
-        color:'#999999'
+        color: '#999999'
     },
-    categ:{
+    categ: {
         display: "flex",
-        justifyContent:'space-around',
-        alignItems:'center'
+        justifyContent: 'space-around',
+        alignItems: 'center'
     },
-    divisor:{
+    divisor: {
         width: Dimensions.get('window').width * 0.9,
         height: Dimensions.get("window").width * 0.005,
         borderRadius: 100,
         marginTop: 10
     },
-    conatinerDate:{
+    conatinerDate: {
         flexDirection: "row"
     },
     describle: {
@@ -308,7 +310,7 @@ const styles = StyleSheet.create({
         paddingHorizontal: Dimensions.get('window').width * 0.06,
         margin: Dimensions.get('window').width * 0.03,
         height: Dimensions.get('window').height * 0.035,
-        width: "auto" ,
+        width: "auto",
         backgroundColor: "#C6D2FF",
         borderRadius: 100,
         display: "flex",
@@ -321,16 +323,16 @@ const styles = StyleSheet.create({
         alignItems: "center",
         padding: 10
     },
-    btnBack:{
-        width: Dimensions.get('window').width ,
+    btnBack: {
+        width: Dimensions.get('window').width,
         height: Dimensions.get("window").height * 0.12,
         backgroundColor: "#fff",
         alignItems: 'flex-end',
-        justifyContent:'center',
-        display:'flex',
+        justifyContent: 'center',
+        display: 'flex',
     },
-    page:{
-        backgroundColor:'#fff'
+    page: {
+        backgroundColor: '#fff'
     }
 
 
