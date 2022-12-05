54ff7a3e163c8252065f967edd38c5bd00002dbf
132anderson <132anderson@gmail.com>
2022-12-04 22:39:31 -0300

    chore: home page

--- web/src/service/index.ts
+++ web/src/service/index.ts
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
 
-const api = axios.create({ baseURL: "https://mediaspaceapi.azurewebsites.net/api" })
+const api = axios.create({ baseURL: "http://localhost:3001/api" })
 
 export default api
