3bde5b295949ac1fb4beeaafeeec9c98f0a111ce
Eduardo Santos Nascimento <eduardosantosnascimento@iMac-de-Eduardo.local>
2022-12-12 08:44:09 -0300

    add icons

--- mobile/src/components/Login/index.tsx
+++ mobile/src/components/Login/index.tsx
@@ -16,8 +16,8 @@ export const Login = ({ navigation }: ILogin) => {
 
     const [isLoad, setIsLoad] = useState(false)
     const [userLogin, setUserLogin] = useState({
-        mail: "edusan3456@gmail.com",
-        password: "123"
+        mail: "",
+        password: ""
     })
     const [hasError, setHasError] = useState(false)
 
